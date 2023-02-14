4ece774ffbaed58011ab80f10bff2c7e78911e91
Lucas, John P <John.P.Lucas@nasa.gov>
2023-02-13 20:47:05 -0500

    [nasa/nos3#97] Updated msgid and default baudrate;

--- fsw/platform_inc/generic_css_platform_cfg.h
+++ fsw/platform_inc/generic_css_platform_cfg.h
@@ -20,7 +20,7 @@
     */
     #define GENERIC_CSS_CFG_STRING           "i2c_2"
     #define GENERIC_CSS_CFG_HANDLE           2 
-    #define GENERIC_CSS_CFG_BAUDRATE_HZ      10000
+    #define GENERIC_CSS_CFG_BAUDRATE_HZ      1000
     #define GENERIC_CSS_CFG_MS_TIMEOUT       250            /* Max 255 */
     /* Note: Debug flag disabled (commented out) by default */
     //#define GENERIC_CSS_CFG_DEBUG
