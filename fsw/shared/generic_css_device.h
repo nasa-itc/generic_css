e4d9c98b09eea2113ffa4dcb80958fe8f2e66a85
rjbrown6 <Robert.J.Brown@nasa.gov>
2025-03-25 12:06:25 -0400

    Revert "style: auto-format via clang-format"
    This reverts commit 645368a6ffebab5bf439eb5e5e7978fea709dd08.

--- fsw/shared/generic_css_device.h
+++ fsw/shared/generic_css_device.h
@@ -12,27 +12,30 @@
 ** Required header files.
 */
 #include "device_cfg.h"
-#include "generic_css_platform_cfg.h"
 #include "hwlib.h"
+#include "generic_css_platform_cfg.h"
+
 
 /*
 ** Type definitions
 */
-#define GENERIC_CSS_I2C_ADDRESS 0x40
-#define GENERIC_CSS_NUM_CHANNELS 6
+#define GENERIC_CSS_I2C_ADDRESS             0x40
+#define GENERIC_CSS_NUM_CHANNELS            6
+
 
 /*
 ** GENERIC_CSS device data telemetry definition
 */
-typedef struct {
-  uint16_t Voltage[GENERIC_CSS_NUM_CHANNELS];
+typedef struct
+{
+    uint16_t Voltage[GENERIC_CSS_NUM_CHANNELS];
 } __attribute__((packed)) GENERIC_CSS_Device_Data_tlm_t;
-#define GENERIC_CSS_DEVICE_DATA_LNGTH sizeof(GENERIC_CSS_Device_Data_tlm_t)
+#define GENERIC_CSS_DEVICE_DATA_LNGTH sizeof ( GENERIC_CSS_Device_Data_tlm_t )
+
 
 /*
 ** Prototypes
 */
-int32_t GENERIC_CSS_RequestData(i2c_bus_info_t *device,
-                                GENERIC_CSS_Device_Data_tlm_t *data);
+int32_t GENERIC_CSS_RequestData(i2c_bus_info_t* device, GENERIC_CSS_Device_Data_tlm_t* data);
 
 #endif /* _GENERIC_CSS_DEVICE_H_ */
