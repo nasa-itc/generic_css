5026b2410e1ef634a524e118b3fb5c7d7e283c88
rjbrown2 <91291114+rjbrown2@users.noreply.github.com>
2025-03-25 16:07:21 +0000

    style: auto-format via clang-format

--- fsw/cfs/src/generic_css_msg.h
+++ fsw/cfs/src/generic_css_msg.h
@@ -12,22 +12,19 @@
 #include "cfe.h"
 #include "generic_css_device.h"
 
-
 /*
 ** Ground Command Codes
 */
-#define GENERIC_CSS_NOOP_CC                 0
-#define GENERIC_CSS_RESET_COUNTERS_CC       1
-#define GENERIC_CSS_ENABLE_CC               2
-#define GENERIC_CSS_DISABLE_CC              3
+#define GENERIC_CSS_NOOP_CC           0
+#define GENERIC_CSS_RESET_COUNTERS_CC 1
+#define GENERIC_CSS_ENABLE_CC         2
+#define GENERIC_CSS_DISABLE_CC        3
 
-
-/* 
+/*
 ** Telemetry Request Command Codes
 */
-#define GENERIC_CSS_REQ_HK_TLM              0
-#define GENERIC_CSS_REQ_DATA_TLM            1
-
+#define GENERIC_CSS_REQ_HK_TLM   0
+#define GENERIC_CSS_REQ_DATA_TLM 1
 
 /*
 ** Generic "no arguments" command type definition
@@ -39,32 +36,30 @@ typedef struct
 
 } GENERIC_CSS_NoArgs_cmd_t;
 
-
 /*
 ** GENERIC_CSS device telemetry definition
 */
-typedef struct 
+typedef struct
 {
-    CFE_MSG_TelemetryHeader_t TlmHeader;
+    CFE_MSG_TelemetryHeader_t     TlmHeader;
     GENERIC_CSS_Device_Data_tlm_t Generic_css;
 
 } __attribute__((packed)) GENERIC_CSS_Device_tlm_t;
-#define GENERIC_CSS_DEVICE_TLM_LNGTH sizeof ( GENERIC_CSS_Device_tlm_t )
-
+#define GENERIC_CSS_DEVICE_TLM_LNGTH sizeof(GENERIC_CSS_Device_tlm_t)
 
 /*
 ** GENERIC_CSS housekeeping type definition
 */
-typedef struct 
+typedef struct
 {
     CFE_MSG_TelemetryHeader_t TlmHeader;
-    uint8   CommandErrorCount;
-    uint8   CommandCount;
-    uint8   DeviceErrorCount;
-    uint8   DeviceCount;
-    uint8   DeviceEnabled;
+    uint8                     CommandErrorCount;
+    uint8                     CommandCount;
+    uint8                     DeviceErrorCount;
+    uint8                     DeviceCount;
+    uint8                     DeviceEnabled;
 
 } __attribute__((packed)) GENERIC_CSS_Hk_tlm_t;
-#define GENERIC_CSS_HK_TLM_LNGTH sizeof ( GENERIC_CSS_Hk_tlm_t )
+#define GENERIC_CSS_HK_TLM_LNGTH sizeof(GENERIC_CSS_Hk_tlm_t)
 
 #endif /* _GENERIC_CSS_MSG_H_ */
