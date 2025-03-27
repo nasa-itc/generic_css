eedf35bf0bc8be7f3001f25181effc321513c3e3
dacarter22 <davidacarter22@gmail.com>
2025-03-27 20:20:18 +0000

    [nasa/nos3#577] Need to fix compairson tests

--- gsw/GENERIC_CSS/procedures/tests/generic_css_cpt_test.rb
+++ gsw/GENERIC_CSS/procedures/tests/generic_css_cpt_test.rb
@@ -113,20 +113,43 @@ initial_device_error_count = tlm("GENERIC_CSS GENERIC_CSS_HK_TLM DEVICE_ERR_COUN
 
 cmd("GENERIC_CSS GENERIC_CSS_REQ_DATA")
 
-css_raw = tlm("GENERIC_CSS GENERIC_CSS_DATA_TLM RAW_CSS_N")
+css_raw0 = tlm("GENERIC_CSS GENERIC_CSS_DATA_TLM RAW_CSS_0")
+css_raw1 = tlm("GENERIC_CSS GENERIC_CSS_DATA_TLM RAW_CSS_1")
+css_raw2 = tlm("GENERIC_CSS GENERIC_CSS_DATA_TLM RAW_CSS_2")
+css_raw3 = tlm("GENERIC_CSS GENERIC_CSS_DATA_TLM RAW_CSS_3")
+css_raw4 = tlm("GENERIC_CSS GENERIC_CSS_DATA_TLM RAW_CSS_4")
+css_raw5 = tlm("GENERIC_CSS GENERIC_CSS_DATA_TLM RAW_CSS_5")
 
-adcs_percent_on = tlm("GENERIC_ADCS GENERIC_ADCS_DI PERCENTONN")
 
-adcs_percent_on_diff = (css_raw - adcs_percent_on).abs()
+
+
+adcs_percent_on0 = tlm("GENERIC_ADCS GENERIC_ADCS_DI PERCENTON0")
+adcs_percent_on1 = tlm("GENERIC_ADCS GENERIC_ADCS_DI PERCENTON1")
+adcs_percent_on2 = tlm("GENERIC_ADCS GENERIC_ADCS_DI PERCENTON2")
+adcs_percent_on3 = tlm("GENERIC_ADCS GENERIC_ADCS_DI PERCENTON3")
+adcs_percent_on4 = tlm("GENERIC_ADCS GENERIC_ADCS_DI PERCENTON4")
+adcs_percent_on5 = tlm("GENERIC_ADCS GENERIC_ADCS_DI PERCENTON5")
+
+
+adcs_percent_on_diff0 = (css_raw0 - adcs_percent_on0).abs()
+adcs_percent_on_diff1 = (css_raw1 - adcs_percent_on1).abs()
+adcs_percent_on_diff2 = (css_raw2 - adcs_percent_on2).abs()
+adcs_percent_on_diff3 = (css_raw3 - adcs_percent_on3).abs()
+adcs_percent_on_diff4 = (css_raw4 - adcs_percent_on4).abs()
+adcs_percent_on_diff5 = (css_raw5 - adcs_percent_on5).abs()
+
 diff_margin = 0.025
 
 wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
 wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
-if css_error == 0
-  wait_check_expression("truth_42_alpha_diff <= diff_margin # #{truth_42_alpha_diff} >= #{diff_margin}", 15)
 
-  wait_check_expression("truth_42_beta_diff <= diff_margin # #{truth_42_beta_diff} >= #{diff_margin}", 15)
-end
+wait_check_expression("adcs_percent_on_diff0 <= diff_margin # #{adcs_percent_on_diff0} >= #{diff_margin}", 15)
+wait_check_expression("adcs_percent_on_diff1 <= diff_margin # #{adcs_percent_on_diff1} >= #{diff_margin}", 15)
+wait_check_expression("adcs_percent_on_diff2 <= diff_margin # #{adcs_percent_on_diff2} >= #{diff_margin}", 15)
+wait_check_expression("adcs_percent_on_diff3 <= diff_margin # #{adcs_percent_on_diff3} >= #{diff_margin}", 15)
+wait_check_expression("adcs_percent_on_diff4 <= diff_margin # #{adcs_percent_on_diff4} >= #{diff_margin}", 15)
+wait_check_expression("adcs_percent_on_diff5 <= diff_margin # #{adcs_percent_on_diff5} >= #{diff_margin}", 15)
+
 
 sleep(5)
 
