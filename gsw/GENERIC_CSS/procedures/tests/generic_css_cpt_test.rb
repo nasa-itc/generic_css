3bb941b05d4ecb2fa68529e49c402b2d0ba8fbb6
dacarter22 <davidacarter22@gmail.com>
2025-03-27 17:23:16 +0000

    [nasa/nos3#577] Copied starter code from fss

--- gsw/GENERIC_CSS/procedures/tests/generic_css_cpt_test.rb
+++ gsw/GENERIC_CSS/procedures/tests/generic_css_cpt_test.rb
@@ -1,6 +1,7 @@
 require 'cosmos'
 require 'cosmos/script'
 require "cfs_lib.rb"
+#require 'math'
 
 ##
 ## NOOP
@@ -10,56 +11,6 @@ initial_error_count = tlm("GENERIC_CSS GENERIC_CSS_HK_TLM CMD_ERR_COUNT")
 initial_device_error_count = tlm("GENERIC_CSS GENERIC_CSS_HK_TLM DEVICE_ERR_COUNT")
 cmd("GENERIC_CSS GENERIC_CSS_NOOP_CC")
 wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM CMD_COUNT > #{initial_command_count}", 30)
-wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
-wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
-
-sleep(5)
-
-##
-## Successful Enable
-##
-initial_command_count = tlm("GENERIC_CSS GENERIC_CSS_HK_TLM CMD_COUNT")
-initial_error_count = tlm("GENERIC_CSS GENERIC_CSS_HK_TLM CMD_ERR_COUNT")
-initial_device_error_count = tlm("GENERIC_CSS GENERIC_CSS_HK_TLM DEVICE_ERR_COUNT")
-cmd("GENERIC_CSS GENERIC_CSS_ENABLE_CC")
-wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM CMD_COUNT > #{initial_command_count}", 30)
-wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
-wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
-wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM DEVICE_ENABLED == 'ENABLED'", 30)
-
-sleep(5)
-
-##
-## Failed Enable (doubled)
-##
-initial_command_count = tlm("GENERIC_CSS GENERIC_CSS_HK_TLM CMD_COUNT")
-initial_error_count = tlm("GENERIC_CSS GENERIC_CSS_HK_TLM CMD_ERR_COUNT")
-initial_device_error_count = tlm("GENERIC_CSS GENERIC_CSS_HK_TLM DEVICE_ERR_COUNT")
-cmd("GENERIC_CSS GENERIC_CSS_ENABLE_CC")
-wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM CMD_COUNT > #{initial_command_count}", 30)
-wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
-wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM DEVICE_ERR_COUNT > #{initial_device_error_count}", 30)
-wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM DEVICE_ENABLED == 'ENABLED'", 30)
-
-sleep(5)
-
-##
-## Housekeeping w/ Device (and verify config)
-##
-initial_error_count = tlm("GENERIC_CSS GENERIC_CSS_HK_TLM CMD_ERR_COUNT")
-initial_device_error_count = tlm("GENERIC_CSS GENERIC_CSS_HK_TLM DEVICE_ERR_COUNT")
-cmd("GENERIC_CSS GENERIC_CSS_REQ_HK")
-wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
-wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
-
-sleep(5)
-
-##
-## Data w/ Device
-##
-initial_error_count = tlm("GENERIC_CSS GENERIC_CSS_HK_TLM CMD_ERR_COUNT")
-initial_device_error_count = tlm("GENERIC_CSS GENERIC_CSS_HK_TLM DEVICE_ERR_COUNT")
-cmd("GENERIC_CSS GENERIC_CSS_REQ_DATA")
 wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
 wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
 
@@ -116,10 +67,88 @@ wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM DEVICE_ERR_COUNT == #{initial_device_
 sleep(5)
 
 ##
+## Successful Enable
+##
+initial_command_count = tlm("GENERIC_CSS GENERIC_CSS_HK_TLM CMD_COUNT")
+initial_error_count = tlm("GENERIC_CSS GENERIC_CSS_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_CSS GENERIC_CSS_HK_TLM DEVICE_ERR_COUNT")
+cmd("GENERIC_CSS GENERIC_CSS_ENABLE_CC")
+wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM CMD_COUNT > #{initial_command_count}", 30)
+wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
+wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM DEVICE_ENABLED == 'ENABLED'", 30)
+
+sleep(5)
+
+##
+## Failed Enable (doubled)
+##
+initial_command_count = tlm("GENERIC_CSS GENERIC_CSS_HK_TLM CMD_COUNT")
+initial_error_count = tlm("GENERIC_CSS GENERIC_CSS_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_CSS GENERIC_CSS_HK_TLM DEVICE_ERR_COUNT")
+cmd("GENERIC_CSS GENERIC_CSS_ENABLE_CC")
+wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM CMD_COUNT > #{initial_command_count}", 30)
+wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM DEVICE_ERR_COUNT > #{initial_device_error_count}", 30)
+wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM DEVICE_ENABLED == 'ENABLED'", 30)
+
+sleep(5)
+
+##
+## Housekeeping w/ Device
+##
+initial_error_count = tlm("GENERIC_CSS GENERIC_CSS_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_CSS GENERIC_CSS_HK_TLM DEVICE_ERR_COUNT")
+cmd("GENERIC_CSS GENERIC_CSS_REQ_HK")
+wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
+
+sleep(5)
+
+##
+## Data w/ Device
+##
+initial_error_count = tlm("GENERIC_CSS GENERIC_CSS_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_CSS GENERIC_CSS_HK_TLM DEVICE_ERR_COUNT")
+
+cmd("GENERIC_CSS GENERIC_CSS_REQ_DATA")
+
+svb0 = tlm("SIM_42_TRUTH SIM_42_TRUTH_DATA SVB_0")
+svb1 = tlm("SIM_42_TRUTH SIM_42_TRUTH_DATA SVB_1")
+svb2 = tlm("SIM_42_TRUTH SIM_42_TRUTH_DATA SVB_2")
+
+css_alpha = tlm("GENERIC_CSS GENERIC_CSS_DATA_TLM GENERIC_CSS_ALPHA")
+css_beta = tlm("GENERIC_CSS GENERIC_CSS_DATA_TLM GENERIC_CSS_BETA")
+css_error = tlm("GENERIC_CSS GENERIC_CSS_DATA_TLM GENERIC_CSS_ERROR_CODE")
+
+truth_42_alpha = -Math.atan2(svb2, svb0)
+truth_42_beta = Math.atan2(svb1, svb0)
+
+truth_42_alpha_diff = (css_alpha - truth_42_alpha).abs()
+truth_42_beta_diff = (css_beta - truth_42_beta).abs()
+diff_margin = 0.025
+
+wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
+if css_error == 0
+  wait_check_expression("truth_42_alpha_diff <= diff_margin # #{truth_42_alpha_diff} >= #{diff_margin}", 15)
+
+  wait_check_expression("truth_42_beta_diff <= diff_margin # #{truth_42_beta_diff} >= #{diff_margin}", 15)
+end
+
+sleep(5)
+
+##
 ## Reset Counters
 ##
+cmd("GENERIC_CSS GENERIC_CSS_DISABLE_CC") # disable to be able to properly check that device count gets reset
 cmd("GENERIC_CSS GENERIC_CSS_RST_COUNTERS_CC")
 wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM CMD_COUNT == 0", 30)
 wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM CMD_ERR_COUNT == 0", 30)
 wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM DEVICE_COUNT == 0", 30)
 wait_check("GENERIC_CSS GENERIC_CSS_HK_TLM DEVICE_ERR_COUNT == 0", 30)
+
+##
+## Reenable so test can be run multiple times
+##
+cmd("GENERIC_CSS GENERIC_CSS_ENABLE_CC")
