543cd4774057e4f29ae8a90a180aab8eda4ea03e
dacarter22 <davidacarter22>
2025-04-03 22:54:42 +0000

    [nasa/nos3#577] converted to johns format

--- gsw/GENERIC_CSS/procedures/generic_css_test.rb
+++ gsw/GENERIC_CSS/procedures/generic_css_test.rb
@@ -1,45 +1,51 @@
 require 'cosmos'
 require 'cosmos/script'
-require 'mission_lib.rb'
+require 'generic_css_lib.rb'
 
-class CSS_LPT < Cosmos::Test
+class GENERIC_CSS_Functional_Test < Cosmos::Test
   def setup
-    
+    safe_generic_css()
   end
 
-  def test_lpt
-    start("tests/generic_css_lpt_test.rb")
+  def test_application
+      start("tests/generic_css_app_test.rb")
+  end
+
+  def test_device
+    start("tests/generic_css_device_test.rb")
   end
 
   def teardown
-
+    safe_generic_css()
   end
 end
 
-class CSS_CPT < Cosmos::Test
-  def setup
-      
+class GENERIC_CSS_Automated_Scenario_Test < Cosmos::Test
+  def setup 
+    safe_generic_css()
   end
 
   def test_cpt
-    start("tests/generic_css_cpt_test.rb")
+      #start("tests/generic_css_ast_test.rb")
   end
 
   def teardown
-
+    safe_generic_css()
   end
 end
 
 class Generic_css_Test < Cosmos::TestSuite
   def initialize
       super()
-      add_test('CSS_CPT')
-      add_test('CSS_LPT')
+      add_test('GENERIC_CSS_Functional_Test')
+      add_test('GENERIC_CSS_Automated_Scenario_Test')
   end
 
   def setup
+    safe_generic_css()
   end
   
   def teardown
+    safe_generic_css()
   end
 end
