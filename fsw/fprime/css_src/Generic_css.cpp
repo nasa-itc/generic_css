a7e2149daa3ec5081c8883714bf565b2d26c8faf
Kushal Parekh <kup9@pitt.edu>
2025-06-09 16:03:03 +0000

    [nasa/nos3#731] fprime css fidelity

--- fsw/fprime/css_src/Generic_css.cpp
+++ fsw/fprime/css_src/Generic_css.cpp
@@ -7,16 +7,7 @@
 #include "css_src/Generic_css.hpp"
 #include "FpConfig.hpp"
 
-extern "C"{
-#include "generic_css_device.h"
-#include "libi2c.h"
-}
 
-#include "nos_link.h"
-
-
-i2c_bus_info_t Generic_CSSI2c;
-GENERIC_CSS_Device_Data_tlm_t Generic_CSSData;
 namespace Components {
 
   // ----------------------------------------------------------------------
@@ -33,6 +24,12 @@ namespace Components {
 
     int32_t status = OS_SUCCESS;
     uint16_t Voltage[GENERIC_CSS_NUM_CHANNELS];
+
+    HkTelemetryPkt.CommandCount = 0;
+    HkTelemetryPkt.CommandErrorCount = 0;
+    HkTelemetryPkt.DeviceCount = 0;
+    HkTelemetryPkt.DeviceErrorCount = 0;
+    HkTelemetryPkt.DeviceEnabled = GENERIC_CSS_DEVICE_DISABLED;
 
     /* Open device specific protocols */
     Generic_CSSI2c.handle = GENERIC_CSS_CFG_HANDLE;
@@ -63,45 +60,184 @@ namespace Components {
   // Handler implementations for commands
   // ----------------------------------------------------------------------
 
+
+  void Generic_css :: NOOP_cmdHandler(FwOpcodeType opCode, U32 cmdSeq){
+    HkTelemetryPkt.CommandCount++;
+
+    this->log_ACTIVITY_HI_TELEM("NOOP command success!");
+    OS_printf("NOOP command successful!\n");
+
+    this->tlmWrite_CommandCount(HkTelemetryPkt.CommandCount);
+    this->cmdResponse_out(opCode, cmdSeq, Fw::CmdResponse::OK);
+  }
+
+  void Generic_css :: ENABLE_cmdHandler(FwOpcodeType opCode, U32 cmdSeq){
+    int32_t status = OS_SUCCESS;
+
+    if(HkTelemetryPkt.DeviceEnabled == GENERIC_CSS_DEVICE_DISABLED)
+    {
+      HkTelemetryPkt.CommandCount++;
+
+      Generic_CSSI2c.handle = GENERIC_CSS_CFG_HANDLE;
+      Generic_CSSI2c.isOpen = PORT_CLOSED;
+      Generic_CSSI2c.speed  = GENERIC_CSS_CFG_BAUDRATE_HZ;
+      Generic_CSSI2c.addr   = GENERIC_CSS_I2C_ADDRESS;
+
+      status = i2c_master_init(&Generic_CSSI2c);
+      if(status == OS_SUCCESS)
+      {
+        HkTelemetryPkt.DeviceEnabled = GENERIC_CSS_DEVICE_ENABLED;
+        HkTelemetryPkt.DeviceCount++;
+
+        this->log_ACTIVITY_HI_TELEM("Enable command success!");
+        OS_printf("Enable command successful!\n");
+      }
+      else
+      {
+        HkTelemetryPkt.DeviceErrorCount++;
+
+        this->log_ACTIVITY_HI_TELEM("Enable command failed to init I2C!");
+        OS_printf("Enable command failed to init I2C!\n");
+      }
+    }
+    else
+    {
+      HkTelemetryPkt.CommandErrorCount++;
+
+      this->log_ACTIVITY_HI_TELEM("Enable failed, already Enabled!");
+      OS_printf("Enable failed, already Enabled!\n");
+    }
+
+    this->tlmWrite_CommandCount(HkTelemetryPkt.CommandCount);
+    this->tlmWrite_CommandErrorCount(HkTelemetryPkt.CommandErrorCount);
+    this->tlmWrite_DeviceCount(HkTelemetryPkt.DeviceCount);
+    this->tlmWrite_DeviceErrorCount(HkTelemetryPkt.DeviceErrorCount);
+    this->tlmWrite_DeviceEnabled(get_active_state(HkTelemetryPkt.DeviceEnabled));
+
+    this->cmdResponse_out(opCode, cmdSeq, Fw::CmdResponse::OK);
+  }
+
+  void Generic_css :: DISABLE_cmdHandler(FwOpcodeType opCode, U32 cmdSeq){
+    int32_t status = OS_SUCCESS;
+
+    if(HkTelemetryPkt.DeviceEnabled == GENERIC_CSS_DEVICE_ENABLED)
+    {
+      HkTelemetryPkt.CommandCount++;
+
+      status = i2c_master_close(&Generic_CSSI2c);
+      if(status == OS_SUCCESS)
+      {
+        HkTelemetryPkt.DeviceEnabled = GENERIC_CSS_DEVICE_DISABLED;
+        HkTelemetryPkt.DeviceCount++;
+
+        this->log_ACTIVITY_HI_TELEM("Disable command success!");
+        OS_printf("Disable command successful!\n");
+      }
+      else
+      {
+        HkTelemetryPkt.DeviceErrorCount++;
+        
+        this->log_ACTIVITY_HI_TELEM("Disable command failed to close I2C!");
+        OS_printf("Disable command failed to close I2C!\n");
+      }
+    }
+    else
+    {
+      HkTelemetryPkt.CommandErrorCount++;
+
+      this->log_ACTIVITY_HI_TELEM("Disable failed, already Disabled!");
+      OS_printf("Disable failed, already Disabled!\n");
+    }
+
+    this->tlmWrite_CommandCount(HkTelemetryPkt.CommandCount);
+    this->tlmWrite_CommandErrorCount(HkTelemetryPkt.CommandErrorCount);
+    this->tlmWrite_DeviceCount(HkTelemetryPkt.DeviceCount);
+    this->tlmWrite_DeviceErrorCount(HkTelemetryPkt.DeviceErrorCount);
+    this->tlmWrite_DeviceEnabled(get_active_state(HkTelemetryPkt.DeviceEnabled));
+
+    this->cmdResponse_out(opCode, cmdSeq, Fw::CmdResponse::OK);
+  }
+
+  void Generic_css :: REQUEST_HOUSEKEEPING_cmdHandler(FwOpcodeType opCode, U32 cmdSeq){
+
+    this->tlmWrite_ADCVoltage0(Generic_CSSData.Voltage[0]);
+    this->tlmWrite_ADCVoltage1(Generic_CSSData.Voltage[1]);
+    this->tlmWrite_ADCVoltage2(Generic_CSSData.Voltage[2]);
+    this->tlmWrite_ADCVoltage3(Generic_CSSData.Voltage[3]);
+    this->tlmWrite_ADCVoltage4(Generic_CSSData.Voltage[4]);
+    this->tlmWrite_ADCVoltage5(Generic_CSSData.Voltage[5]);
+
+    this->tlmWrite_CommandCount(HkTelemetryPkt.CommandCount);
+    this->tlmWrite_CommandErrorCount(HkTelemetryPkt.CommandErrorCount);
+    this->tlmWrite_DeviceCount(HkTelemetryPkt.DeviceCount);
+    this->tlmWrite_DeviceErrorCount(HkTelemetryPkt.DeviceErrorCount);
+    this->tlmWrite_DeviceEnabled(get_active_state(HkTelemetryPkt.DeviceEnabled));
+
+    this->log_ACTIVITY_HI_TELEM("Requested Housekeeping!");
+    OS_printf("Requested Housekeeping!\n");
+
+    this->cmdResponse_out(opCode, cmdSeq, Fw::CmdResponse::OK);
+  }
+
+  void Generic_css :: RESET_COUNTERS_cmdHandler(FwOpcodeType opCode, U32 cmdSeq){
+    HkTelemetryPkt.CommandCount = 0;
+    HkTelemetryPkt.CommandErrorCount = 0;
+    HkTelemetryPkt.DeviceCount = 0;
+    HkTelemetryPkt.DeviceErrorCount = 0;
+
+    this->log_ACTIVITY_HI_TELEM("Reset Counters command successful!");
+    OS_printf("Reset Counters command successful!\n");
+    this->tlmWrite_CommandCount(HkTelemetryPkt.CommandCount);
+    this->tlmWrite_CommandErrorCount(HkTelemetryPkt.CommandErrorCount);
+    this->tlmWrite_DeviceCount(HkTelemetryPkt.DeviceCount);
+    this->tlmWrite_DeviceErrorCount(HkTelemetryPkt.DeviceErrorCount);
+
+    this->cmdResponse_out(opCode, cmdSeq, Fw::CmdResponse::OK);
+  }
+
  // GENERIC_CSS_RequestData
  void Generic_css :: REQUEST_DATA_cmdHandler(FwOpcodeType opCode, U32 cmdSeq) {
 
   int32_t status = OS_SUCCESS;
-  uint16_t  ADCVoltage0;
-  uint16_t  ADCVoltage1;
-  uint16_t  ADCVoltage2;
-  uint16_t  ADCVoltage3;
-  uint16_t  ADCVoltage4;
-  uint16_t  ADCVoltage5;
-  uint16_t  ADCVoltage6;
-  uint16_t  ADCVoltage7;
 
   status = GENERIC_CSS_RequestData(&Generic_CSSI2c, &Generic_CSSData);
   if (status == OS_SUCCESS)
   {
-    this->log_ACTIVITY_HI_TELEM("GENERIC_CSS command success\n");
+    this->log_ACTIVITY_HI_TELEM("Request Data command success\n");
+    OS_printf("RequestData command successful!\n");
+
   }
   else
   {
-    this->log_ACTIVITY_HI_TELEM("GENERIC_CSS command failed!\n");
+    this->log_ACTIVITY_HI_TELEM("Request Data command failed!\n");
+    OS_printf("RequestData command failed!\n");
   }
 
-  ADCVoltage0 = Generic_CSSData.Voltage[0];
-  ADCVoltage1 = Generic_CSSData.Voltage[1];
-  ADCVoltage2 = Generic_CSSData.Voltage[2];
-  ADCVoltage3 = Generic_CSSData.Voltage[3];
-  ADCVoltage4 = Generic_CSSData.Voltage[4];
-  ADCVoltage5 = Generic_CSSData.Voltage[5];
-
-  this->tlmWrite_ADCVoltage0(ADCVoltage0);
-  this->tlmWrite_ADCVoltage1(ADCVoltage1);
-  this->tlmWrite_ADCVoltage2(ADCVoltage2);
-  this->tlmWrite_ADCVoltage3(ADCVoltage3);
-  this->tlmWrite_ADCVoltage4(ADCVoltage4);
-  this->tlmWrite_ADCVoltage5(ADCVoltage5);
+  this->tlmWrite_ADCVoltage0(Generic_CSSData.Voltage[0]);
+  this->tlmWrite_ADCVoltage1(Generic_CSSData.Voltage[1]);
+  this->tlmWrite_ADCVoltage2(Generic_CSSData.Voltage[2]);
+  this->tlmWrite_ADCVoltage3(Generic_CSSData.Voltage[3]);
+  this->tlmWrite_ADCVoltage4(Generic_CSSData.Voltage[4]);
+  this->tlmWrite_ADCVoltage5(Generic_CSSData.Voltage[5]);
 
   // Tell the fprime command system that we have completed the processing of the supplied command with OK status
   this->cmdResponse_out(opCode, cmdSeq, Fw::CmdResponse::OK);
+}
+
+inline Generic_css_ActiveState Generic_css :: get_active_state(uint8_t DeviceEnabled)
+{
+  Generic_css_ActiveState state;
+
+  if(DeviceEnabled == GENERIC_CSS_DEVICE_ENABLED)
+  {
+    state.e = Generic_css_ActiveState::ENABLED;
+  }
+  else
+  {
+    state.e = Generic_css_ActiveState::DISABLED;
+  }
+
+  return state;
 }
 
 
--- fsw/fprime/css_src/Generic_css.hpp
+++ fsw/fprime/css_src/Generic_css.hpp
@@ -8,6 +8,27 @@
 #define Components_Generic_css_HPP
 
 #include "css_src/Generic_cssComponentAc.hpp"
+#include "css_src/Generic_css_ActiveStateEnumAc.hpp"
+
+extern "C"{
+#include "generic_css_device.h"
+#include "libi2c.h"
+}
+
+#include "nos_link.h"
+
+typedef struct
+{
+    uint8_t                         DeviceCount;
+    uint8_t                         DeviceErrorCount;
+    uint8_t                         CommandErrorCount;
+    uint8_t                         CommandCount;
+    uint8_t                         DeviceEnabled;
+} CSS_Hk_tlm_t;
+#define CSS_HK_TLM_LNGTH sizeof(CSS_Hk_tlm_t)
+
+#define GENERIC_CSS_DEVICE_DISABLED 0
+#define GENERIC_CSS_DEVICE_ENABLED  1
 
 namespace Components {
 
@@ -16,6 +37,10 @@ namespace Components {
   {
 
     public:
+
+    i2c_bus_info_t Generic_CSSI2c;
+    GENERIC_CSS_Device_Data_tlm_t Generic_CSSData;
+    CSS_Hk_tlm_t HkTelemetryPkt;
 
       // ----------------------------------------------------------------------
       // Component construction and destruction
@@ -42,10 +67,38 @@ namespace Components {
       //     FwOpcodeType opCode, //!< The opcode
       //     U32 cmdSeq //!< The command sequence number
       // ) override;
+
+      void NOOP_cmdHandler(
+        FwOpcodeType opCode,
+        U32 cmdSeq
+      ) override;
+
+      void ENABLE_cmdHandler(
+        FwOpcodeType opCode,
+        U32 cmdSeq
+      ) override;
+
+      void DISABLE_cmdHandler(
+        FwOpcodeType opCode,
+        U32 cmdSeq
+      ) override;
+
+      void REQUEST_HOUSEKEEPING_cmdHandler(
+        FwOpcodeType opCode,
+        U32 cmdSeq
+      ) override;
+
+      void RESET_COUNTERS_cmdHandler(
+        FwOpcodeType opCode,
+        U32 cmdSeq
+      ) override;
+
       void REQUEST_DATA_cmdHandler(
         FwOpcodeType opCode, 
         U32 cmdSeq
       ) override;
+
+      inline Generic_css_ActiveState get_active_state(uint8_t DeviceEnabled);
 
   };
 
